922b9c5e797ed628cce32df8a312f2c415a3432f
jakilgallen <jkilgallen@ucsb.edu>
2022-02-07 08:57:38 -0800

    edited layout, added tabs, putting widgets under different tabs

--- ESM244_Shiny_Project/app.R
+++ ESM244_Shiny_Project/app.R
@@ -2,6 +2,8 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(bslib)
+library(ggplot2)
+library(shinythemes)
 
 ### Choose theme 
 my_theme <- bs_theme(
@@ -11,52 +13,84 @@ my_theme <- bs_theme(
   base_font = font_google("Space Mono")
 )
 
+ui <- 
+  navbarPage("Gender Equality", theme = my_theme,
+             tabPanel("Home","Summary of what this whole app is all about"),
+             tabPanel("Statistics by Region", "Quick summary on state of affairs",
+                      fluidPage(
+                        tabsetPanel(
+                          tabPanel("Women's Empowerment Score", br(),
+                                   sidebarLayout(
+                                     sidebarPanel(
+                                       selectInput(inputId = "pt_color",
+                                                   label = "Choose World Region",
+                                                   choices = c("North America",
+                                                               "Central America", 
+                                                               "South America",
+                                                               "Asia",
+                                                               "Europe",
+                                                               "Africa")),
+                                       radioButtons(inputId = "gender_data",
+                                                    label = "Choose Variables",
+                                                    choices = c("Year", 
+                                                                "Income", 
+                                                                "Education")),
+                                     ), # end sidebar panel
+                                     mainPanel(plotOutput(outputId = "gender_map")) # end mainpanel
+                                   ) # end sidebarLayout
+                                   ) # end tabPanel "women empowerment score"
+                          
+                        )
+                      )),
+             tabPanel("Interactive Map", "Here you can see an interactive world map aint it pretty?"),
+             tabPanel("Slider of GE Index", "Here you can see an interactive world map aint it pretty?"),
+             tabPanel("Scatter Plot", "lookie here its a plot that you can play with"))
 ### create user interface:
-ui <- fluidPage(theme = my_theme,
-  titlePanel(h1("Gender Inequality", align = "center")),
-  sidebarLayout(
-    sidebarPanel(selectInput(inputId = "pt_color",
-                             label = "Choose Region",
-                             choices = c("North America",
-                                         "Central America", 
-                                         "South America",
-                                         "Asia",
-                                         "Europe",
-                                         "Africa")),
-                 radioButtons(inputId = "gender_data",
-                              label = "Choose Variables:",
-                              choices = c("Year", 
-                                          "Income", 
-                                          "Education")),
-    ), # end sidebar panel
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Home", "Summary of what this whole app is all about"),
-                  tabPanel("Statement of the Problem-Stats by region", "Quick summary on state of affairs"),
-                  tabPanel("Interactive Map", "Here you can see an interactive world map aint it pretty?"),
-                  tabPanel("Slider of GE Index", "Here you can see an interactive world map aint it pretty?"),
-                  tabPanel("Scatter Plot", "lookie here its a plot that you can play with")),
-              plotOutput(outputId = "gender_map")), # end mainPanel
-    
-   ), # end sidebarLayout
-    
-  fluidRow(
-    column(4,
-           
-           # Copy the line below to make a slider range 
-           sliderInput("slider2", label = h3("Slider Range"), min = 0, 
-                       max = 100, value = c(40, 60))
-    )
-  ),
-  
-  hr(),
-  
-  fluidRow(
-    column(4, verbatimTextOutput("value")),
-    column(4, verbatimTextOutput("range"))
-  )
-  
-)
+# ui <- fluidPage(theme = my_theme,
+#   titlePanel(h1("Gender Inequality", align = "center")),
+#   sidebarLayout(
+#     sidebarPanel(selectInput(inputId = "pt_color",
+#                              label = "Choose Region",
+#                              choices = c("North America",
+#                                          "Central America", 
+#                                          "South America",
+#                                          "Asia",
+#                                          "Europe",
+#                                          "Africa")),
+#                  radioButtons(inputId = "gender_data",
+#                               label = "Choose Variables:",
+#                               choices = c("Year", 
+#                                           "Income", 
+#                                           "Education")),
+#     ), # end sidebar panel
+#     mainPanel(
+#       tabsetPanel(type = "tabs", # could also use navlist instead if want to put tabs on the side
+#                   tabPanel("Home", "Summary of what this whole app is all about"),
+#                   tabPanel("Statistics by region", "Quick summary on state of affairs"),
+#                   tabPanel("Interactive Map", "Here you can see an interactive world map aint it pretty?"),
+#                   tabPanel("Slider of GE Index", "Here you can see an interactive world map aint it pretty?"),
+#                   tabPanel("Scatter Plot", "lookie here its a plot that you can play with")),
+#               plotOutput(outputId = "gender_map")), # end mainPanel
+#     
+#    ), # end sidebarLayout
+#     
+#   fluidRow(
+#     column(4,
+#            
+#            # Copy the line below to make a slider range 
+#            sliderInput("slider2", label = h3("Slider Range"), min = 0, 
+#                        max = 100, value = c(40, 60))
+#     )
+#   ),
+#   
+#   hr(),
+#   
+#   fluidRow(
+#     column(4, verbatimTextOutput("value")),
+#     column(4, verbatimTextOutput("range"))
+#   )
+#   
+# )
 
 ### create server function:
 server <- function(input, output) {
