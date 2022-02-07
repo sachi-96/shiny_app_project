f0582a2cfbcb718a6fb6ad81c64beff84487a133
jakilgallen <jkilgallen@ucsb.edu>
2022-02-07 09:50:39 -0800

    adding photos and text to tabs

--- ESM244_Shiny_Project/app.R
+++ ESM244_Shiny_Project/app.R
@@ -10,12 +10,15 @@ my_theme <- bs_theme(
   bg = '#0b3d91',
   fg = 'white',
   primary = '#FCC780',
-  base_font = font_google("Space Mono")
+  base_font = font_google("Righteous")
 )
 
 ui <- 
-  navbarPage("Gender Equality", theme = my_theme,
-             tabPanel("Home","Summary of what this whole app is all about"),
+  navbarPage("Understanding the State of Gender Equality Globally", theme = my_theme,
+             tabPanel("Home",
+                      "While Gender Equality has been identified as a global goal, 
+                      progress towards Gender Equality remains slow with evidence of backsliding across some key indicators see: https://www.nytimes.com/2019/12/04/us/domestic-violence-international.html. 
+                      The purpose of this application is to increase awareness of the current state of affairs across regions and countries to promote positive change."),
              tabPanel("Statistics by Region", "Quick summary on state of affairs", # Can change later to make drop down with different outcomes to chose from
                       fluidPage(
                         tabsetPanel(
@@ -36,7 +39,9 @@ ui <-
                                                                 "Income", 
                                                                 "Education")),
                                      ), # end sidebar panel
-                                     mainPanel(plotOutput(outputId = "gender_map")) # end mainpanel
+                                     mainPanel(
+                                       img(src = "gender-page_v-08.jpeg", height = 350, width = 350),
+                                       plotOutput(outputId = "gender_map")) # end mainpanel
                                    ) # end sidebarLayout
                                    ), # end tabPanel "women empowerment score"
                           tabPanel("Acceptance of IPV", br(),
@@ -73,52 +78,6 @@ ui <-
                       ), # end tab panel "Slider of ge"
              tabPanel("Scatter Plot", "lookie here its a plot that you can play with"))
              
-### create user interface:
-# ui <- fluidPage(theme = my_theme,
-#   titlePanel(h1("Gender Inequality", align = "center")),
-#   sidebarLayout(
-#     sidebarPanel(selectInput(inputId = "pt_color",
-#                              label = "Choose Region",
-#                              choices = c("North America",
-#                                          "Central America", 
-#                                          "South America",
-#                                          "Asia",
-#                                          "Europe",
-#                                          "Africa")),
-#                  radioButtons(inputId = "gender_data",
-#                               label = "Choose Variables:",
-#                               choices = c("Year", 
-#                                           "Income", 
-#                                           "Education")),
-#     ), # end sidebar panel
-#     mainPanel(
-#       tabsetPanel(type = "tabs", # could also use navlist instead if want to put tabs on the side
-#                   tabPanel("Home", "Summary of what this whole app is all about"),
-#                   tabPanel("Statistics by region", "Quick summary on state of affairs"),
-#                   tabPanel("Interactive Map", "Here you can see an interactive world map aint it pretty?"),
-#                   tabPanel("Slider of GE Index", "Here you can see an interactive world map aint it pretty?"),
-#                   tabPanel("Scatter Plot", "lookie here its a plot that you can play with")),
-#               plotOutput(outputId = "gender_map")), # end mainPanel
-#     
-#    ), # end sidebarLayout
-#     
-#   fluidRow(
-#     column(4,
-# 
-#            # Copy the line below to make a slider range
-#            sliderInput("slider2", label = h3("Slider Range"), min = 0,
-#                        max = 100, value = c(40, 60))
-#     )
-#   ),
-# #   
-#   hr(),
-#   
-#   fluidRow(
-#     column(4, verbatimTextOutput("value")),
-#     column(4, verbatimTextOutput("range"))
-#   )
-#   
-# )
 
 ### create server function:
 server <- function(input, output) {
