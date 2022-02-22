e22315ac7792a483f23651d7e441251665bb5148
jakilgallen <jkilgallen@ucsb.edu>
2022-02-21 16:09:40 -0800

    remade format of page, created reactive data table but needs to be cleaned up

--- ESM244_Shiny_Project/app.R
+++ ESM244_Shiny_Project/app.R
@@ -26,33 +26,258 @@ library(lubridate)
 library(rAmCharts)
 library(rsconnect)
 library(shinydashboard)
+library(DT)
 
 # Read in the data
-# gender_data <- read_xlsx(here("data", "Gender.xlsx"))
-# 
-# # Tidying the data
-# gender_mod <- gender_data %>%
-#   rename("HDI Rank" = ...1,
-#          "Country" = ...2,
-#          "Gender Equality Index '18" = ...3,
-#          "Rank '18" = ...5,
-#          "Maternal Mortality Ratio '15" = SDG3.1,
-#          "Adolescent Birth Rate '15-'20" = SDG3.7,
-#          "Seats in Parliment '18" = SDG5.5,
-#          "Secondary Education (F)'10-'18" = SDG4.6,
-#          "Secondary Education (M)'10-'18" = ...15,
-#          "Labour Force Participation (F)'18" = ...17,
-#          "Labour Force Participation (M)'18" = ...19) %>%
-#   select(-...4,-...6,-...8,-...10,-...12,-...14,-...16,-...18,-...20) %>%
-#   filter(!row_number() %in% c(1, 2, 3, 4, 5, 228:261))
+gender_data <- read_xlsx(here("data", "Gender.xlsx"))
 
-### Choose theme 
-app_theme <- bs_theme(
-  bg = '#0b3d91',
-  fg = 'white',
-  primary = '#FCC780',
-  base_font = font_google("Righteous")
-)
+# # Tidying the data
+gender_mod <- gender_data %>%
+  rename("HDI Rank" = ...1,
+         "Country" = ...2,
+         "Gender Equality Index '18" = ...3,
+         "Rank '18" = ...5,
+         "Maternal Mortality Ratio '15" = SDG3.1,
+         "Adolescent Birth Rate '15-'20" = SDG3.7,
+         "Seats in Parliment '18" = SDG5.5,
+         "Secondary Education (F)'10-'18" = SDG4.6,
+         "Secondary Education (M)'10-'18" = ...15,
+         "Labour Force Participation (F)'18" = ...17,
+         "Labour Force Participation (M)'18" = ...19) %>%
+  select(-...4,-...6,-...8,-...10,-...12,-...14,-...16,-...18,-...20) %>%
+  filter(!row_number() %in% c(1, 2, 3, 4, 5, 228:261))
+
+## Creating a subset of the data to use to make the map
+map_data <- gender_mod %>% 
+  janitor::clean_names()
+
+## dropping rows that do not contain countries
+map_data <- map_data %>% slice(-c(193, 200:222))
+## need to drop some more--- 
+map_data <- map_data %>% slice(-c(63, 118, 156))
+
+## now putting in alphabetical order
+map_data <- map_data %>% arrange(country)
+# Adding ISO3 codes to country data to use as join key
+ISO3 <- c("AFG",
+          "ALB",
+          "DZA",
+          "AGO",
+          "ATG",
+          "ARG",
+          "ARM",
+          "AUS",
+          "AUT",
+          "AZE",
+          "BHS",
+          "BHR",
+          "BGD",
+          "BRB",
+          "BlR",
+          "BEL",
+          "BLZ",
+          "BEN",
+          "BTN",
+          "BOL",
+          "BIH",
+          "BWA",
+          "BRA",
+          "BRN",
+          "BGR",
+          "BFA",
+          "BDI",
+          "KHM",
+          "CMR",
+          "CAN",
+          "CPV",
+          "CAF",
+          "TCD",
+          "CHL",
+          "CHN",
+          "COL",
+          "COM",
+          "COG",
+          "COD",
+          "CRI",
+          "CIV",
+          "HRV",
+          "CUB",
+          "CYP",
+          "CZE",
+          "DNK",
+          "DJI",
+          "DMA",
+          "DOM",
+          "ECU",
+          "EGY",
+          "SLV",
+          "GNQ",
+          "ERI",
+          "EST",
+          "ETH",
+          "FJI",
+          "FIN",
+          "FRA",
+          "GAB",
+          "GEO",
+          "DEU",
+          "GHA",
+          "GRC",
+          "GRD",
+          "GTM",
+          "GIN",
+          "GNB",
+          "GUY",
+          "HTI",
+          "HND",
+          "HKG",
+          "HUN",
+          "ISL",
+          "IND",
+          "IDN",
+          "IRN",
+          "IRQ",
+          "IRL",
+          "ISR",
+          "PSE",
+          "ITA",
+          "JAM",
+          "JPN",
+          "JOR",
+          "KAZ",
+          "KEN",
+          "KIR",
+          "KOS",
+          "KWT",
+          "KGZ",
+          "LAO",
+          "LVA",
+          "LBN",
+          "LSO",
+          "LBR",
+          "LBY",
+          "LIE",
+          "LTU",
+          "LUX",
+          "MKD",
+          "MDG",
+          "MWI",
+          "MYS",
+          "MDV",
+          "MLI",
+          "MLT",
+          "MHL",
+          "MRT",
+          "MUS",
+          "MEX",
+          "FSM",
+          "MDA",
+          "MCO",
+          "MNG",
+          "MNE",
+          "MAR",
+          "MOZ",
+          "MMR",
+          "NAM",
+          "NRU",
+          "NPL",
+          "NLD",
+          "NZL",
+          "NIC",
+          "NER",
+          "NGA",
+          "PRK",
+          "NOR",
+          "OMN",
+          "PAK",
+          "PLW",
+          "PAN",
+          "PNG",
+          "PRY",
+          "PER",
+          "PHL",
+          "POL",
+          "PRT",
+          "QAT",
+          "ROU",
+          "RUS",
+          "RWA",
+          "KNA",
+          "LCA",
+          "VCT",
+          "WSM",
+          "SMR",
+          "STP",
+          "SAU",
+          "SEN",
+          "SRB",
+          "SYC",
+          "SLE",
+          "SGP",
+          "SVK",
+          "SVN",
+          "SLB",
+          "SOM",
+          "ZAF",
+          "KOR",
+          "SON",
+          "ESP",
+          "LKA",
+          "SON",
+          "SUR",
+          "SWZ",
+          "SWE",
+          "CHE",
+          "SYR",
+          "TWN",
+          "TJK",
+          "TZA",
+          "THA",
+          "GMB",
+          "TLS",
+          "TGO",
+          "TON",
+          "TTO",
+          "TUN",
+          "TUR",
+          "TKM",
+          "TUV",
+          "UGA",
+          "UKR",
+          "ARE",
+          "GBR",
+          "USA",
+          "URY",
+          "UZB",
+          "VUT",
+          "VEN",
+          "VNM",
+          "PSE",
+          "YEM",
+          "ZMB",
+          "ZWE") 
+
+## cant seem to get this to work I shall return
+# i <- rep(ISO3, each = 80)
+# map_data <- map_data %>% mutate("ISO3" = ISO3)
+# pfg <- pfg %>% rename(ISO3 = i)
+
+# Creating data used to rank country press freedoms
+
+map.rankings <- map_data$
+
+# Creating the Map Data
+# pfgts <- pfg %>% filter(measure %in% "Total_Score")
+# pal <- colorBin("YlOrRd", domain = pfgts$score, bins = 5)
+
+
+
+
+# Interactive Data Table
+map_data_table <- map_data %>%
+  group_by(country, gender_equality_index_18) 
+
+map_data_table$Status <- NULL
 
 ## Dashboard
 header <- dashboardHeader(title = "Gender Equality")
@@ -87,7 +312,8 @@ body <- dashboardBody(
             fluidRow(
               box(title = "Statistics by World Region", 
                   h1("Different stats across world regions"),
-                  p("Here you can see a Quick summary on state of affairs, i.e. women's empowerment, IPV, etc."))
+                  p("Here you can see a Quick summary on state of affairs, i.e. women's empowerment, IPV, etc."),
+                  DTOutput('table'))
             )),
     tabItem(tabName = "map",
             fluidRow(
@@ -131,6 +357,14 @@ body <- dashboardBody(
     
   ) # end tabItems
 ) # end dashboardBody
+
+### Choose theme 
+app_theme <- bs_theme(
+  bg = '#0b3d91',
+  fg = 'white',
+  primary = '#FCC780',
+  base_font = font_google("Righteous")
+)
 
 ## define UI for application
 ui <- dashboardPage(header, sidebar, body, skin = "blue")
@@ -291,25 +525,53 @@ ui <- dashboardPage(header, sidebar, body, skin = "blue")
 ### create server function:
 server <- function(input, output) { ### start server function
   
+  ## Interactive map function---- need to edit to make work for my data
+  # observe({
+  #   if(!is.null(input$year)){
+  #     map <- joinCountryData2Map(selected(), joinCode = "ISO3",
+  #                                nameJoinColumn = "ISO3")
+  #     leafletProxy("worldmap", data = map) %>%
+  #       addTiles() %>% 
+  #       clearShapes() %>% 
+  #       addPolygons(fillColor = ~pal(map$score),
+  #                   weight = 2,
+  #                   opacity = 1,
+  #                   color = "white",
+  #                   dashArray = "3",
+  #                   fillOpacity = 0.7,
+  #                   highlight = highlightOptions(
+  #                     weight = 5,
+  #                     color = "white",
+  #                     dashArray = "3",
+  #                     fillOpacity = .8,
+  #                     bringToFront = TRUE),
+  #                   label = ~paste(as.character(map$country),
+  #                                  "Total Index Score: ", as.character(map$score)))
+  #   }})
   
-  # output$info <- renderText({
-  #   xy_str <- function(e) {
-  #     if(is.null(e)) return("NULL\n")
-  #     paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-  #   }
-  #   xy_range_str <- function(e) {
-  #     if(is.null(e)) return("NULL\n")
-  #     paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
-  #            " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
-  #   }
-  #   
-  #   paste0(
-  #     "click: ", xy_str(input$plot_click),
-  #     "dblclick: ", xy_str(input$plot_dblclick),
-  #     "hover: ", xy_str(input$plot_hover),
-  #     "brush: ", xy_range_str(input$plot_brush)
-  #   )
-  # }) ### end scatter plot
+  output$table <- renderDT({
+    map_data_table
+  })
+  
+  ## Scatterplot function
+  output$info <- renderText({
+    xy_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+    }
+    xy_range_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1),
+             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+    }
+
+    paste0(
+      "click: ", xy_str(input$plot_click),
+      "dblclick: ", xy_str(input$plot_dblclick),
+      "hover: ", xy_str(input$plot_hover),
+      "brush: ", xy_range_str(input$plot_brush)
+    )
+  }) ### end scatter plot
 } ### end server function 
 
 ### combine into an app:
